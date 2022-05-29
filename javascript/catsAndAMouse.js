c1e77c35bfaff2b71bb708d48c2c671edd7752c5
Amy-j-seo <amywonderland77@gmail.com>
2022-05-29 14:34:13 +0900

    cat!

--- javascript/catsAndAMouse.js
+++ javascript/catsAndAMouse.js
@@ -1,11 +1,24 @@
 function catAndMouse(x, y, z) {
-  const catA = z - x;
-  const catB = z - y;
-
-  if (catA > catB) {
-    return catB;
-  } else if (catB > catA) {
-    return catA;
+  let catA;
+  if (x > z) {
+    catA = x - z;
   } else {
+    catA = z - x;
+  }
+  let catB;
+  if (y > z) {
+    catB = y - z;
+  } else {
+    catB = z - y;
+  }
+  console.log(catA, "===", catB);
+  if (catA > catB) {
+    return "Cat B";
+  } else if (catB > catA) {
+    return "Cat A";
+  } else {
+    return "Mouse C";
   }
 }
+
+console.log(catAndMouse(47, 30, 6));
